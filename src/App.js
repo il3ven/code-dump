e12ad93ae33334299fb61d8f117432b9519c8b1f
Vaibhav Chanana <vaibhavchanana@yahoo.co.in>
2020-11-17 01:35:15 +0530

    Prototyped Code Mirror

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Controlled as CodeMirror } from "react-codemirror2";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/monokai.css";
+import "codemirror/theme/elegant.css";
+import "codemirror/theme/eclipse.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // componentDidMount() {
+  //   const editor = document.getElementById("editor");
+
+  //   var myCodeMirror = CodeMirror(
+  //     function (elt) {
+  //       editor.parentNode.replaceChild(elt, editor);
+  //     },
+  //     { lineNumbers: true, value: editor.value, mode: "javascript" }
+  //   );
+  // }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "function myScript(){console.log('Hello');}",
+      options: {
+        theme: "monokai",
+      },
+    };
+  }
+
+  handleButton = () => {
+    const theme =
+      this.state.options.theme === "monokai" ? "eclipse" : "monokai";
+    this.setState({ options: { theme: theme } });
+    console.log("Handle Button");
+  };
+
+  render() {
+    console.log(this.state.value);
+
+    return (
+      <div>
+        <CodeMirror
+          value={this.state.value}
+          options={this.state.options}
+          onBeforeChange={(editor, data, value) => {
+            this.setState({ value });
+          }}
+          onChange={(editor, data, value) => {}}
+        />
+        <button onClick={this.handleButton}>Change Theme</button>
+      </div>
+    );
+  }
 }
 
 export default App;
