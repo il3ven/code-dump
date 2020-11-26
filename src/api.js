a62f7166ed47091c59ef5ccd606aa3e0fbc7fdcb
Vaibhav Chanana <vaibhavchanana@yahoo.co.in>
2020-11-26 22:07:26 +0530

    Deleted files from root and shifted them

--- src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const postDump = (content) => {
-  return axios.post("https://ovpce.sse.codesandbox.io/create", {
-    content: content,
-  });
-};
-
-export { postDump };
--- src/utils.js
+++
@@ -1,6 +0,0 @@
-const checkClipPermission = async () => {
-  const queryOpts = { name: "clipboard-read", allowWithoutGesture: true };
-  return navigator.permissions.query(queryOpts);
-};
-
-export { checkClipPermission };
