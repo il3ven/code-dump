ced468830d5e28d998b8d3c4cf8239b6fa453fc6
Vaibhav Chanana <vaibhavchanana@yahoo.co.in>
2020-12-05 20:50:00 +0530

    Changed api.js

--- react/src/api.js
+++ react/src/api.js
@@ -1,54 +1,56 @@
 import startInput from "./static/startInput";
-// import axios from "axios";
+import axios from "axios";
 
-// const postDump = (code, langKey) => {
-//   const data = { code: code };
-//   if (langKey) data.langKey = langKey;
+const BASE_URL = "";
 
-//   return axios.post("http://127.0.0.1:8080/create", data);
-// };
+const postDump = (code, langKey) => {
+  const data = { code: code };
+  if (langKey) data.langKey = langKey;
 
-// /*
-//   {
-//     code: string
-//     language: language_key
-//   }
-// */
-// const getDump = (id) => {
-//   return axios.get(`http://127.0.0.1:8080/read/${id}`);
-// };
-
-const ids = [
-  "X57wdEw2jj3ZKmPa",
-  "A17wdEw2jj3ZKmQw",
-  "B27wdEw2jj3ZKmC1",
-  "C37wdEw2jj3ZKmDb",
-];
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-const postDump = (code) => {
-  console.log("Dump Posted");
-  return new Promise((resolve, reject) => {
-    const data = {
-      id: ids[getRandomInt(4)],
-    };
-
-    setTimeout(() => resolve(data), 500);
-  });
+  return axios.post(`${BASE_URL}/create`, data);
 };
 
+/*
+  {
+    code: string
+    language: language_key
+  }
+*/
 const getDump = (id) => {
-  console.log("Getting Dump");
-  return new Promise((resolve, reject) => {
-    const data = {
-      code: startInput,
-    };
-
-    setTimeout(() => resolve(data), 500);
-  });
+  return axios.get(`${BASE_URL}/${id}`);
 };
+
+// const ids = [
+//   "X57wdEw2jj3ZKmPa",
+//   "A17wdEw2jj3ZKmQw",
+//   "B27wdEw2jj3ZKmC1",
+//   "C37wdEw2jj3ZKmDb",
+// ];
+
+// function getRandomInt(max) {
+//   return Math.floor(Math.random() * Math.floor(max));
+// }
+
+// const postDump = (code) => {
+//   console.log("Dump Posted");
+//   return new Promise((resolve, reject) => {
+//     const data = {
+//       id: ids[getRandomInt(4)],
+//     };
+
+//     setTimeout(() => resolve(data), 500);
+//   });
+// };
+
+// const getDump = (id) => {
+//   console.log("Getting Dump");
+//   return new Promise((resolve, reject) => {
+//     const data = {
+//       code: startInput,
+//     };
+
+//     setTimeout(() => resolve(data), 500);
+//   });
+// };
 
 export { postDump, getDump };
