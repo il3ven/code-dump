c4026617dd4b6ae11dde88f832ca93355a3ef3e4
Vaibhav Chanana <vaibhavchanana@yahoo.co.in>
2020-12-29 18:10:44 +0530

    Fix: App crashed on clicking on theme

--- src/components/input.js
+++ src/components/input.js
@@ -163,7 +163,7 @@ class InputBar extends React.Component {
         <OptionsDiv>
           {this.state.filteredOptions.map((opt, index) => {
             const props = {
-              key: opt.ext[0] || opt.key,
+              key: index,
               value: index,
               onClick: (event) => {
                 this.props.handleSubmit(
